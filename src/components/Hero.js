379a04c087b3b266eecf3c1a3c9d7755b35f327b
Qspc <shibgotalloh_qqq@apps.ipb.ac.id>
2021-11-22 14:08:33 +0700

    cek

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,13 +4,8 @@ const Hero = () => {
   return (
     <div className="bg-black">
       <div className="flex flex-col items-center justify-center max-w-5xl mx-auto text-white h-96 md:min-h-screen">
-        <h1 className="mb-4 text-2xl text-center md:mb-12 md:text-7xl">
-          Karena Film yang seru harus diabadikan
-        </h1>
-        <Link
-          className="px-2 py-1 text-xs bg-blue-700 rounded-full md:px-6 md:py-2 md:text-2xl"
-          to="/film"
-        >
+        <h1 className="mb-4 text-2xl text-center md:mb-12 md:text-7xl">Karena Film yang seru harus diabadikan</h1>
+        <Link className="px-2 py-1 text-xs bg-blue-700 rounded-full md:px-6 md:py-2 md:text-2xl" to="/film">
           Cek Film Disini
         </Link>
       </div>
