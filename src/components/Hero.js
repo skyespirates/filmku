36e5c8b9463df0cae95de18f53b0d57f9127c122
Qspc <shibgotalloh_qqq@apps.ipb.ac.id>
2021-12-03 23:00:46 +0700

    progress

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
-    <div className="bg-black">
+    <div className="" style={{ backgroundImage: 'image1.jpg', backgroundSize: '1440px 640px', backgroundColor: '#1B2124' }}>
       <div className="flex flex-col items-center justify-center max-w-5xl mx-auto text-white h-96 md:min-h-screen">
-        <h1 className="mb-4 text-2xl text-center md:mb-12 md:text-7xl">Karena Film yang seru harus diabadikan</h1>
-        <Link className="px-2 py-1 text-xs bg-blue-700 rounded-full md:px-6 md:py-2 md:text-2xl" to="/film">
+        <h1 className="mb-16 text-2xl text-center md:mb-12 md:text-7xl font-bold tracking-wide leading-relaxed ">Karena Film yang seru harus diabadikan</h1>
+        <Link className="px-3 py-1 font-semibold text-xs bg-blue-700 rounded-full md:px-9 md:py-3 md:text-2xl" to="/film" style={{ backgroundColor: '#04A3DD' }}>
           Cek Film Disini
         </Link>
       </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,32 +7,21 @@ const Navbar = () => {
   const page = location.pathname.split("/")[1];
   const [toggle, setToggle] = useState(false);
   return (
-    <div className="bg-black">
-      <div
-        onClick={() => setToggle(!toggle)}
-        className="relative flex flex-col px-2 py-2 mx-auto cursor-pointer max-w-7xl md:flex-row md:justify-between"
-      >
-        <Link
-          id="logo"
-          className="text-2xl font-bold text-blue-700 md:text-4xl"
-          to="/"
-        >
+    <div className="bg-black" style={{ backgroundColor: '#1B2124' }}>
+      <div onClick={() => setToggle(!toggle)} className="relative flex flex-col px-2 py-6 mx-auto cursor-pointer max-w-7xl md:flex-row md:justify-between">
+        <Link id="logo" className="text-2xl font-bold text-blue-700 md:text-4xl" style={{ color: '#04A3DD' }} to="/">
           FILMKU
         </Link>
         <div className="absolute z-10 flex items-center justify-center w-6 h-6 rounded-full md:hidden top-3 right-3">
           <ion-icon size="large" name="menu-outline"></ion-icon>
         </div>
-        <ul
-          className={`text-sm md:text-base items-center justify-center md:justify-end  w-full  md:flex ${
-            toggle ? "block" : "hidden"
-          }`}
-        >
+        <ul className={`text-sm md:text-base items-center justify-center md:justify-end  w-full  md:flex ${toggle ? 'block' : 'hidden'}`}>
           <li className="text-center md:mr-9">
-            <Link className="block py-1 text-white md:py-0 " to="/">
+            <Link className="block py-1 text-white md:py-0 hover:text-blue-300 hover:font-bold" to="/">
               Home
             </Link>
           </li>
-          {page === "film" ? null : (
+          {page === 'film' ? null : (
             <>
               <li className="text-center md:mr-9">
                 <a className="block py-1 text-white md:py-0 " href="#about">
@@ -47,10 +36,7 @@ const Navbar = () => {
             </>
           )}
           <li className="">
-            <Link
-              className="block px-4 py-1 text-center text-white bg-blue-700 rounded-full"
-              to="/film"
-            >
+            <Link className="block px-4 py-1 text-center text-white bg-blue-700 rounded-full" style={{ backgroundColor: '#04A3DD' }} to="/film">
               Daftar Film
             </Link>
           </li>
