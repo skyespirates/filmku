2675ef26f88027546d8439235a34f8075833372c
Qspc <shibgotalloh_qqq@apps.ipb.ac.id>
2021-12-06 23:01:11 +0700

    progress

--- src/components/Fitur.js
+++ src/components/Fitur.js
@@ -1,12 +1,20 @@
 const Fitur = () => {
   return (
-    <div
-      id="feature"
-      className="flex items-center justify-between max-w-5xl mx-auto"
-    >
-      <div className="w-64 h-64 bg-gray-500"></div>
-      <div className="w-64 h-64 bg-gray-500"></div>
-      <div className="w-64 h-64 bg-gray-500"></div>
+    <div id="feature" className="flex items-center justify-between max-w-5xl mx-auto">
+      <div className="grid justify-items-center w-50 h-56 bg-transparent">
+        <div className="w-40 h-36 rounded rounded-xl" style={{ backgroundColor: '#1B2124' }}></div>
+        <div className="mt-6 text-center text-4xl font-semibold	">Berbagai Genre</div>
+      </div>
+      <div className="grid justify-items-center w-50 h-56 bg-transparent">
+        <div className="w-40 h-36 rounded rounded-xl" style={{ backgroundColor: '#1B2124' }}></div>
+        <div className="mt-6 text-center text-4xl font-semibold	">Periode Beragam</div>
+      </div>
+      <div className="grid justify-items-center w-50 h-56 bg-transparent">
+        <div className="w-40 h-36 rounded rounded-xl" style={{ backgroundColor: '#1B2124' }}></div>
+        <div className="mt-6 text-center text-4xl font-semibold	">Negara yang berbeda</div>
+      </div>
+      {/* <div className="w-40 h-36 rounded rounded-xl" style={{ backgroundColor: '#1B2124' }}></div>
+      <div className="w-40 h-36 rounded rounded-xl" style={{ backgroundColor: '#1B2124' }}></div> */}
     </div>
   );
 };
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,12 +2,16 @@ import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
-    <div className="" style={{ backgroundImage: 'image1.jpg', backgroundSize: '1440px 640px', backgroundColor: '#1B2124' }}>
-      <div className="flex flex-col items-center justify-center max-w-5xl mx-auto text-white h-96 md:min-h-screen">
-        <h1 className="mb-16 text-2xl text-center md:mb-12 md:text-7xl font-bold tracking-wide leading-relaxed ">Karena Film yang seru harus diabadikan</h1>
-        <Link className="px-3 py-1 font-semibold text-xs bg-blue-700 rounded-full md:px-9 md:py-3 md:text-2xl" to="/film" style={{ backgroundColor: '#04A3DD' }}>
-          Cek Film Disini
-        </Link>
+    <div>
+      <div style={{ backgroundImage: 'url("image1.jpg")', backgroundSize: '1440px 640px', zIndex: '10' }}>
+        <div className="" style={{ backgroundColor: '#1B2124', opacity: '0.6', zIndex: '2' }}>
+          <div className="flex flex-col items-center justify-center max-w-5xl mx-auto text-white h-96 md:min-h-screen">
+            <h1 className="mb-16 text-2xl text-center md:mb-12 md:text-7xl font-bold tracking-wide leading-relaxed ">Karena Film yang seru harus diabadikan</h1>
+            <Link className="px-3 py-1 font-semibold text-xs bg-blue-700 rounded-full md:px-9 md:py-3 md:text-2xl" to="/film" style={{ backgroundColor: '#04A3DD' }}>
+              Cek Film Disini
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
