40dd39003e9c707973945ce2b30b8af2282b6644
Shibgotalloh Sabilana <shibgotalloh.sabilana@prosa.ai>
2022-10-30 21:03:04 +0700

    migration api

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
-const movieRoutes = require("./routes/movieRoutes");
-const { seedDB, cleanDB } = require("./helpers");
+const movieRoutes = require('./routes/movieRoutes');
+const { seedDB, cleanDB } = require('./helpers');
 
 const app = express();
 
 app.use(express.json());
-app.use("/api/v1/movies", movieRoutes);
+app.use(cors());
+app.use('/api/v1/movies', movieRoutes);
 
 // seedDB();
 // cleanDB();
@@ -17,8 +19,8 @@ mongoose
   .connect(process.env.DB_URI)
   .then(() => {
     app.listen(process.env.PORT, () => {
-      console.log("Connected to database");
-      console.log("Server is running on port " + process.env.PORT);
+      console.log('Connected to database');
+      console.log('Server is running on port ' + process.env.PORT);
     });
   })
   .catch((error) => console.log(error));
