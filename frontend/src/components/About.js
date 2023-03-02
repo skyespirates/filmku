cc7e82c2bc2e790dc2c6927b4c70f6c6a486ae88
skyespirates <akashiseijurou414@gmail.com>
2023-03-02 13:24:43 +0700

    new: menambahkan ant design

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,3 +1,5 @@
+import { Button } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
 const About = () => {
   return (
     <div id="about" className="">
@@ -5,8 +7,25 @@ const About = () => {
         <h1 className="mb-12 text-5xl text-center font-bold">Tentang</h1>
         <div className="flex">
           <div className="flex-1 text-3xl leading-10 text-justify mx-14	">
-            <p className="mb-6">Filmku merupakan platform informatif yang menjelaskan seputar review dan pendapat dari berbagai film di dunia.</p>
-            <p className="">Pembahasan film berasal dari berbagai negara dengan genre yang beragam. adapun jajak pendapat yang dimuat merupakan pendapat pribadi</p>
+            <p className="mb-6">
+              Filmku merupakan platform informatif yang menjelaskan seputar
+              review dan pendapat dari berbagai film di dunia.
+            </p>
+            <p className="">
+              Pembahasan film berasal dari berbagai negara dengan genre yang
+              beragam. adapun jajak pendapat yang dimuat merupakan pendapat
+              pribadi
+            </p>
+            {/* <Button type="primary">Button</Button>
+            <Button
+              type="primary"
+              icon={<DownloadOutlined />}
+              size="large"
+              shape="round"
+              id="btn-dl"
+            >
+              Download
+            </Button> */}
           </div>
           <div className="flex-1">
             <img className="" src="image2.jpg" alt="gambar" />
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,42 +1,67 @@
-import { Link } from 'react-router-dom';
-import { useLocation } from 'react-router';
-import { useState } from 'react';
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router";
+import { useState } from "react";
 
 const Navbar = () => {
   const location = useLocation();
-  const page = location.pathname.split('/')[1];
+  const page = location.pathname.split("/")[1];
   const [toggle, setToggle] = useState(false);
   return (
-    <div className="bg-black" style={{ backgroundColor: '#1B2124' }}>
-      <div onClick={() => setToggle(!toggle)} className="relative flex flex-col px-2 py-6 mx-auto cursor-pointer max-w-7xl md:flex-row md:justify-between">
-        <Link id="logo" className="text-2xl font-bold text-blue-700 md:text-4xl" style={{ color: '#04A3DD' }} to="/">
+    <div className="bg-black" style={{ backgroundColor: "#1B2124" }}>
+      <div
+        onClick={() => setToggle(!toggle)}
+        className="relative flex flex-col px-2 py-6 mx-auto cursor-pointer max-w-7xl md:flex-row md:justify-between"
+      >
+        <Link
+          id="logo"
+          className="text-2xl font-bold text-blue-700 md:text-4xl"
+          style={{ color: "#04A3DD" }}
+          to="/"
+        >
           FILMKU
         </Link>
         <div className="absolute z-10 flex items-center justify-center w-6 h-6 rounded-full md:hidden top-3 right-3">
           <ion-icon size="large" name="menu-outline"></ion-icon>
         </div>
-        <ul className={`text-sm md:text-base items-center justify-center md:justify-end  w-full  md:flex ${toggle ? 'block' : 'hidden'}`}>
+        <ul
+          className={`text-sm md:text-base items-center justify-center md:justify-end  w-full  md:flex ${
+            toggle ? "block" : "hidden"
+          }`}
+        >
           <li className="text-center md:mr-9">
-            <Link className="block py-1 text-white md:py-0 hover:text-blue-300 hover:font-bold" to="/">
+            <Link
+              className="block py-1 text-white md:py-0 hover:text-blue-300 hover:font-bold"
+              to="/"
+            >
               Home
             </Link>
           </li>
-          {page === 'film' ? null : (
+          {page === "film" ? null : (
             <>
               <li className="text-center md:mr-9">
-                <a className="block py-1 text-white md:py-0 hover:text-blue-300" href="#about">
+                <a
+                  className="block py-1 text-white md:py-0 hover:text-blue-300"
+                  href="#about"
+                >
                   Tentang
                 </a>
               </li>
               <li className="text-center md:mr-9">
-                <a className="block py-1 text-white md:py-0 hover:text-blue-300" href="#team">
+                <a
+                  className="block py-1 text-white md:py-0 hover:text-blue-300"
+                  href="#team"
+                >
                   Kontak
                 </a>
               </li>
             </>
           )}
           <li className="">
-            <Link className="block px-4 py-1 text-center text-white bg-blue-700 rounded-full hover:bg-transparent" style={{ backgroundColor: '#04A3DD' }} to="/film">
+            <Link
+              className="block px-4 py-1 text-center text-white bg-blue-700 rounded-full hover:bg-transparent"
+              style={{ backgroundColor: "#1890ff" }}
+              to="/film"
+            >
               Daftar Film
             </Link>
           </li>
